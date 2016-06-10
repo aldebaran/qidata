2c6e4e0fcd62f0a54c2a27f93c1f4d7b10126e9f
sambrose <sambrose@aldebaran.com>
2016-06-10 15:30:50 +0200

    Rework the subcommands tree and import

--- qidata/commands/main.py
+++ qidata/commands/main.py
@@ -3,19 +3,44 @@
 # Argparse
 import argparse
 # qidata
-from . import annotate, status, xmp
 import qidata.version
 
 DESCRIPTION = "Manage data-sets"
-SUBCOMMANDS = [status.StatusCommand,
-               annotate.AnnotateCommand,
-               xmp.XMPCommand]
+SUBCOMMANDS = []
+
+try:
+	from annotator.commands import main as AnnotationMain
+	SUBCOMMANDS.append([AnnotationMain, "annotate"])
+except:
+	pass
+
+try:
+	from qidata_file.commands import main as QiDataFileMain
+	SUBCOMMANDS.append([QiDataFileMain, "file"])
+except:
+	pass
+
+try:
+	from qidata_objects.commands import main as ObjectsMain
+	SUBCOMMANDS.append([ObjectsMain, "objects"])
+except:
+	pass
+
+try:
+	from xmp.commands import main as XmpMain
+	SUBCOMMANDS.append([XmpMain, "xmp"])
+except:
+	pass
 
 def parser():
 	parser = argparse.ArgumentParser(description=DESCRIPTION)
 	subparsers = parser.add_subparsers()
 	for sc in SUBCOMMANDS:
-		sc.add_parser(subparsers)
+		sub_parser = subparsers.add_parser(sc[1],
+		                                      description=sc[0].DESCRIPTION,
+		                                      help=sc[0].DESCRIPTION)
+		sc[0].make_command_parser(sub_parser)
+
 	parser.add_argument("-v", "--version", action=qidata.version.VersionAction, nargs=0,
 	                    help="print qidata release version number")
 	return parser
