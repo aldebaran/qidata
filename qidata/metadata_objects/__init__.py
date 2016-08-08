f9e637d4a5d84ae24f91f77afce8ad8aeddebe3b
sambrose <sambrose@aldebaran.com>
2016-08-08 18:16:48 +0200

    metadata_objects subpackage
    Former qidataObjects are now named metadataObjects as they represent
    metadata information (as opposed to data).
    
    Inner package stuff (commands, version action) are removed

---
+++ qidata/metadata_objects/__init__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+"""
+This package contains different classes representing metadata.
+"""
+
+from person import Person
+from face import Face
+from typedlist import TypedList
+
+def makeMetadataObject(metadata_object_name, data = None):
+    """
+    MetadataObjects factory
+    This is the prefered way to create MetadataObjects. Objects that
+    can be created by this method are the ones in `qidata.types.metadataTypes`
+
+    @metadata_object_name : requested object to build's name (str)
+    @data           : data to prefill to created object (dict)
+    """
+    if metadata_object_name == "Person":
+        return Person() if data is None else Person.fromDict(data)
+    elif metadata_object_name == "Face":
+        return Face() if data is None else Face.fromDict(data)
+    else:
+        raise TypeError("Required annotation item (%s) does not exist"%metadata_object_name)
+
+def printHelp(metadata_object_name):
+    """
+    Print some help on the requested QiDataObject
+
+    @metadata_object_name : object name on which help is requested
+    """
+    if metadata_object_name == "Person":
+        help(Person)
+    elif metadata_object_name == "Face":
+        help(Face)
+    else:
+        raise TypeError("Required annotation item (%s) does not exist"%metadata_object_name)
+
+# ––––––––––––––––––––––––––––
+# List of available data types
+
+DataObjectTypes = ["Face", "Person"]
+
+
+#––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––#
