044c876334f0fb40524f34e01c97ebcade81eaa6
sambrose <sambrose@aldebaran.com>
2017-07-27 10:04:55 +0200

    Prepare packaging with a better version handling

--- setup.py
+++ setup.py
@@ -6,11 +6,18 @@ import os
 
 CONTAINING_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
-package_list = find_packages(where=CONTAINING_DIRECTORY)
+try:
+    from utils import get_version_from_tag
+    __version__ = get_version_from_tag()
+    open(os.path.join(CONTAINING_DIRECTORY,"qidata/VERSION"), "w").write(__version__)
+except ImportError:
+    __version__=open(os.path.join(CONTAINING_DIRECTORY,"qidata/VERSION")).read().split()[0]
+
+package_list = find_packages(where=os.path.join(CONTAINING_DIRECTORY))
 
 setup(
     name='qidata',
-    version="1.0.0-a",
+    version=__version__,
     description='Metadata annotation tool',
     long_description=open(os.path.join(CONTAINING_DIRECTORY,'README.rst')).read(),
     url='https://gitlab.aldebaran.lan/qidata/qidata',
@@ -33,6 +40,7 @@ setup(
         "xmp >= 0.3",
         "qidata_devices >= 0.0.3",
     ],
+    package_data={"qidata":["VERSION"]},
     scripts=['bin/qidata'],
     entry_points={
         'qidata.metadata.definition': [
